973d87c6fd92ebef9a0956712fecc1b0fed661cb
VasanthiKuppuswamy <Vasanthik@gmx.net>
2025-10-31 11:13:29 +0100

    gitignore file created and API key stored in .env

--- data_fetcher.py
+++ data_fetcher.py
@@ -1,7 +1,8 @@
 import requests
-
-
-API_KEY = "5xmjyS7QSqo8Bb0AsHDw6w==1QzdbDyMawRuJvlb"
+import os
+from dotenv import load_dotenv
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
 def fetch_data(animal_name):
     """
     Fetches the animals data for the animal 'animal_name'.
